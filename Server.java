b134fe6874f5f6b7c2f78a229d81b501c0b1735e
Alexandra Harrison <apharri3@live.unc.edu>
2015-07-11 18:46:37 -0400

    Added UI

--- Server.java
+++ Server.java
@@ -1,8 +1,14 @@
+package server;
+
 import java.net.*;
 import java.util.*;
 import java.io.*;
+import javax.swing.*;
+import java.awt.*;
 
-public class Server {
+public class Server extends JFrame {
+	
+	JTextArea chatBox = new JTextArea();
 	
 	public static void main(String args[]) {
 		new Server();
@@ -10,9 +16,18 @@ public class Server {
 	
 	public Server() {
 		
+		setLayout(new BorderLayout());
+		chatBox.setEditable(false);
+		add(new JScrollPane(chatBox), BorderLayout.CENTER);
+		setTitle("Chat App");
+		setSize(550, 400);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		
+		
 		try {
 			ServerSocket connectSocket = new ServerSocket(4000);
-			System.out.println("Server connected at: " + new Date());
+			chatBox.append("Server connected at: " + new Date());
 			
 			while(true) {
 				Socket socket = connectSocket.accept();
@@ -21,7 +36,7 @@ public class Server {
 			} 
 		} catch (IOException exception) {
 				System.out.println("Error" + exception);
-		}
+			}
 		
 	}
 
@@ -41,9 +56,11 @@ public class ClientThread implements Runnable {
 				BufferedReader socketInput = new BufferedReader(new InputStreamReader(threadSocket.getInputStream()));
 				
 				serverOutput.println("You have connected at: " + new Date());
+				chatBox.append("Client connected\n");
 				
 				while(true) {
 					String chatInput = socketInput.readLine();
+					chatBox.append(chatInput +"\n"); 
 					System.out.println(chatInput);
 				}
 			} catch (IOException exception) {
